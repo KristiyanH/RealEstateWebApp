ada60b0617d5530fde21da8bb1e144aeb424815f
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-04-15 20:21:35 +0300

    implemented few controller tests for Bookings Controller (33.68%)

---
+++ RealEstateWebApp.Tests/Controllers/BookingsControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using MyTested.AspNetCore.Mvc;
+using RealEstateWebApp.Controllers;
+using RealEstateWebApp.Data.Models;
+using RealEstateWebApp.ViewModels.Bookings;
+using RealEstateWebApp.ViewModels.Properties;
+using System;
+using Xunit;
+
+namespace RealEstateWebApp.Tests.Controllers
+{
+    public class BookingsControllerTests
+    {
+        [Fact]
+        public void BookGetShouldBeForAuthorizedUsersAndReturnReturnView()
+           => MyController<BookingsController>
+           .Instance()
+           .Calling(c => c.Book())
+           .ShouldHave()
+           .ActionAttributes(attributes => attributes
+           .RestrictingForAuthorizedRequests())
+           .AndAlso()
+           .ShouldReturn()
+           .View();
+
+        [Theory]
+        [InlineData(
+            "Kristiyan Hristov",
+            "0876065511",
+            "kristiyan.a.hristov@gmail.com",
+            "17.07.2022 17:30",
+            "Any Description",
+            2
+            )]
+        public void BookPostShouldBeForAuthorizedUsersAndWithCorrectData(string fullName,
+            string phoneNumber,
+            string email,
+            string visitDate,
+            string description,
+            int propertyId)
+            => MyController<BookingsController>
+            .Instance()
+            .Calling(c => c.Book(new BookVisitFormModel()
+            {
+                FullName = fullName,
+                PhoneNumber = phoneNumber,
+                Email = email,
+                VisitDate = visitDate,
+                Description = description
+            }, propertyId))
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+            .RestrictingForHttpMethod(HttpMethod.Post)
+            .RestrictingForAuthorizedRequests())
+            .ValidModelState();
+
+        [Fact]
+
+        public void BookPostShouldBeForAuthorizedUsersAndReturnErrorViewWitIncorrectData()
+             => MyController<BookingsController>
+            .Instance()
+            .Calling(c => c.Book(With.Any<BookVisitFormModel>(), 50))
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+            .RestrictingForHttpMethod(HttpMethod.Post)
+            .RestrictingForAuthorizedRequests())
+            .ValidModelState()
+            .AndAlso()
+            .ShouldReturn()
+            .View("Error");
+
+
+    }
+}

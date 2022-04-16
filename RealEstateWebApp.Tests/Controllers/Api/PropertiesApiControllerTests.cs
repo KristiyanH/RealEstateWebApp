03d7224dea8485661235e7a334f859226a008bad
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-04-16 14:04:18 +0300

    Properties API controller tests done

---
+++ RealEstateWebApp.Tests/Controllers/Api/PropertiesApiControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using MyTested.AspNetCore.Mvc;
+using RealEstateWebApp.Areas.Manager.Controllers;
+using RealEstateWebApp.Controllers.Api;
+using RealEstateWebApp.Data.Models;
+using System.Collections.Generic;
+using Xunit;
+using static RealEstateWebApp.Tests.Data.Properties;
+namespace RealEstateWebApp.Tests.Controllers.Api
+{
+    public class PropertiesApiControllerTests
+    {
+        [Fact]
+        public void GetPropertiesShouldReturnListOfCorrectModel()
+            => MyController<PropertiesApiController>
+            .Instance()
+            .WithData(TenProperties())
+            .Calling(c => c.GetProperties())
+            .ShouldReturn()
+            .ResultOfType<IEnumerable<Property>>();
+
+        [Fact]
+        public void GetPropertyDetailsShouldReturnCorrectModel()
+            => MyController<PropertiesApiController>
+            .Instance()
+            .WithData(TenProperties())
+            .Calling(c => c.GetPropertyDetails(1))
+            .ShouldReturn()
+            .ResultOfType<Property>();
+    }
+}

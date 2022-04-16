b61fbccc3f897388e89ebbb4c8688f2f4c3784c0
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-04-16 15:26:35 +0300

    All tests done

--- RealEstateWebApp.Tests/Controllers/UsersControllerTests.cs
+++ RealEstateWebApp.Tests/Controllers/UsersControllerTests.cs
@@ -1,6 +1,33 @@
-﻿namespace RealEstateWebApp.Tests.Controllers
+﻿using MyTested.AspNetCore.Mvc;
+using RealEstateWebApp.Areas.Manager.Controllers;
+using RealEstateWebApp.Controllers;
+using RealEstateWebApp.Data.Models;
+using RealEstateWebApp.ViewModels.Bookings;
+using static RealEstateWebApp.Tests.Data.Users;
+using Xunit;
+using RealEstateWebApp.Areas.Manager.Models.Users;
+using System.Collections.Generic;
+
+namespace RealEstateWebApp.Tests.Controllers
 {
     public class UsersControllerTests
     {
+        [Fact]
+        public void UsersControllerShouldBeForAuthorizedUsers()
+            => MyController<UsersController>
+            .Instance()
+            .ShouldHave()
+            .Attributes(attributes => attributes
+            .RestrictingForAuthorizedRequests());
+
+        [Fact]
+        public void AllUsersShouldReturnViewWithCorrectModel()
+            => MyController<UsersController>
+            .Instance()
+            .WithData(TenUsers())
+            .Calling(c => c.AllUsers())
+            .ShouldReturn()
+            .View(v => v
+            .WithModelOfType<List<AllUsersViewModel>>());
     }
 }
